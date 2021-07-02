3b9610c80f1847287ad10a8feae8469edc14be0d
Renan Gonçalves Vanni <renanvanni13@gmail.com>
2021-07-02 16:00:11 -0300

    Implementação do consumo do backend na pasta Tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,13 +11,35 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  tema: Tema = new Tema()
+  listaTema: Tema[]
+
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
 
   ngOnInit() {
 
-    if(environment.token == ''){
+    if (environment.token == '') {
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTemas()
   }
 
+  findAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTema = resp
+    })
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
+  }
 }
