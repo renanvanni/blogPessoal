dbe1c6442985a0bcc2c85f0c001e479446d89d48
Renan Gonçalves Vanni <renanvanni13@gmail.com>
2021-07-12 11:13:28 -0300

    Alertas reutilizaveis implementados

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { User } from '../model/User';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
@@ -25,7 +26,13 @@ export class InicioComponent implements OnInit {
   user: User = new User()
   idUser = environment.id
 
-  constructor(private router: Router, private postagemService: PostagemService, private temaService: TemaService, private auth: AuthService) { }
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService, 
+    private temaService: TemaService, 
+    private auth: AuthService,
+    private alertas: AlertasService
+    ) { }
 
   ngOnInit() {
 
@@ -73,7 +80,7 @@ export class InicioComponent implements OnInit {
 
     this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
       this.postagem = resp
-      alert('Postagem realizada com sucesso! 😃')
+      this.alertas.showAlertSuccess('Postagem realizada com sucesso! 😃')
       this.postagem = new Postagem()
       this.getAllPostagens()
     })
